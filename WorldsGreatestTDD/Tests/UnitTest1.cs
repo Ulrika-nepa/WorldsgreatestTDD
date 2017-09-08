1300b1adf20aa560ef0a84d4584bd21b47cb11ed
Ulrika Malmgren <ulrika.malmgren@nepa.com>
2017-09-08 16:11:57 +0200

    First implementation

--- WorldsGreatestTDD/Tests/UnitTest1.cs
+++ WorldsGreatestTDD/Tests/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
 {
@@ -9,7 +8,48 @@ namespace Tests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsFalse(true);
+            var a = new Fraction(0);
+            var b = new Fraction(0);
+            var c = new Fraction(0);
+
+            var calculator = new Calculator();
+            var result = calculator.Add(a, b);
+            
+            Assert.AreEqual(c, result);
         }
+    }
+
+    internal class Fraction
+    {
+        private int v;
+
+        public Fraction(int v)
+        {
+            this.v = v;
+        }
+        public override bool Equals(object o)
+        {
+            var other = o as Fraction;
+            return v == other.v;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        public Fraction Add(Fraction fraction, Fraction fraction1)
+        {
+            return new Fraction(0);
+        }
+
+
     }
 }
