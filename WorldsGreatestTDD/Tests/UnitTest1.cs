7c44cf0606d8afa438a19adcd047b5054e693721
Ulrika Malmgren <ulrika.malmgren@nepa.com>
2017-10-13 15:27:59 +0200

    Simplified

--- WorldsGreatestTDD/Tests/UnitTest1.cs
+++ WorldsGreatestTDD/Tests/UnitTest1.cs
@@ -8,28 +8,27 @@ namespace Tests
         [TestMethod]
         public void multiply_zeros()
         {
-            Assert.AreEqual(new Fraction(0).Nominator, new Fraction(0).Multiply(new Fraction(0)).Nominator);
+            Assert.AreEqual(0, new Fraction(0).Multiply(new Fraction(0)).Nominator);
         }
 
         [TestMethod]
         public void multiply_whole_numbers()
         {
-            Assert.AreEqual(new Fraction(2).Nominator, new Fraction(1).Multiply(new Fraction(2)).Nominator);
+            Assert.AreEqual(2, new Fraction(1).Multiply(new Fraction(2)).Nominator);
         }
 
         [TestMethod]
         public void multiply_factions()
         {
-            Assert.AreEqual(new Fraction(1,2).Nominator, new Fraction(1).Multiply(new Fraction(1,2)).Nominator);
-            Assert.AreEqual(new Fraction(1, 2).Denominator, new Fraction(1).Multiply(new Fraction(1,2)).Denominator);
+            Assert.AreEqual(1, new Fraction(1).Multiply(new Fraction(1,2)).Nominator);
+            Assert.AreEqual(2, new Fraction(1).Multiply(new Fraction(1,2)).Denominator);
         }
 
         [TestMethod]
         public void multiply_factions_with_negative_nominator()
         {
-            Assert.AreEqual(new Fraction(-6, 72).Nominator, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Nominator);
-            Assert.AreEqual(new Fraction(-6, 72).Denominator, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Denominator);
-
+            Assert.AreEqual(-6, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Nominator);
+            Assert.AreEqual(72, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Denominator);
         }
     }
 
