6f95b81eb15a5ab970517101935da2aacecb3674
Ulrika Malmgren <ulrika.malmgren@nepa.com>
2017-10-13 15:24:29 +0200

    Check that the denominator is the same as well

--- WorldsGreatestTDD/Tests/UnitTest1.cs
+++ WorldsGreatestTDD/Tests/UnitTest1.cs
@@ -21,29 +21,32 @@ namespace Tests
         public void multiply_factions()
         {
             Assert.AreEqual(new Fraction(1,2).Nominator, new Fraction(1).Multiply(new Fraction(1,2)).Nominator);
+            Assert.AreEqual(new Fraction(1, 2).Denominator, new Fraction(1).Multiply(new Fraction(1,2)).Denominator);
         }
 
         [TestMethod]
         public void multiply_factions_with_negative_nominator()
         {
             Assert.AreEqual(new Fraction(-6, 72).Nominator, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Nominator);
+            Assert.AreEqual(new Fraction(-6, 72).Denominator, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Denominator);
+
         }
     }
 
     public class Fraction
     {
         public int Nominator;
-        private readonly int denominator;
+        public readonly int Denominator;
 
         public Fraction(int nominator, int denominator = 1)
         {
             Nominator = nominator;
-            this.denominator = denominator;
+            this.Denominator = denominator;
         }
 
         public Fraction Multiply(Fraction operand)
         {
-            return new Fraction(Nominator*operand.Nominator, denominator*operand.denominator);
+            return new Fraction(Nominator*operand.Nominator, Denominator*operand.Denominator);
         }
     }
 
