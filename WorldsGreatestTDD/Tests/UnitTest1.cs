26ee8551e5b0aa550fe866ce796a22961275780b
Ulrika Malmgren <ulrika.malmgren@nepa.com>
2017-10-13 15:21:51 +0200

    Multiply with negative nominator

--- WorldsGreatestTDD/Tests/UnitTest1.cs
+++ WorldsGreatestTDD/Tests/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.Design;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests
 {
@@ -21,9 +20,14 @@ namespace Tests
         [TestMethod]
         public void multiply_factions()
         {
-            Assert.AreEqual(new Fraction(1,2).Nominator, new Fraction(1,1).Multiply(new Fraction(1,2)).Nominator);
+            Assert.AreEqual(new Fraction(1,2).Nominator, new Fraction(1).Multiply(new Fraction(1,2)).Nominator);
         }
 
+        [TestMethod]
+        public void multiply_factions_with_negative_nominator()
+        {
+            Assert.AreEqual(new Fraction(-6, 72).Nominator, new Fraction(-3,8).Multiply(new Fraction(2, 9)).Nominator);
+        }
     }
 
     public class Fraction
